e52b460672d5aad4fd639eb4ad55d22f73a4f144
U <19878020+0x556c79@users.noreply.github.com>
2024-09-29 02:25:31 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 if [ ! -f /opt/monero/data/bitmonero.conf ]; then
   cp /opt/monero/bitmonero.conf /opt/monero/bitmonero.conf
 fi
-exec ./monerod --non-interactive --config-file=/opt/monero/.bitmonero/bitmonero.conf
+exec monerod --non-interactive --config-file=/opt/monero/.bitmonero/bitmonero.conf
